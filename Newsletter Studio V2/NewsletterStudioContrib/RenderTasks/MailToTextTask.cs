78d8092705cf6c332aa53b33f0c3f291e90c054e
Olivier ROMAND <oliv.rm@gmail.com>
2017-01-31 10:11:29 +0100

    Added Task to convert to text

---
+++ Newsletter Studio V2/NewsletterStudioContrib/RenderTasks/MailToTextTask.cs
@@ -0,0 +1,38 @@
+﻿using NewsletterStudio.Services.RenderTasks;
+using NewsletterStudioContrib.RenderTasks.Utils.Peche33Task;
+using System;
+
+namespace NewsletterStudioContrib.RenderTasks
+{
+    class MailToTextTask : RenderTask
+    {
+        public override void ProcessPreRender(RenderResult renderResult, RenderTaskParameters parameters)
+        {
+            renderResult.MessageBody = renderResult.MessageBody.Replace("unexpectedtoken", "");
+        }
+
+
+        public override void ProcessUniqueItem(RenderResult renderResult, RenderTaskUniqueItemParameters parameters)
+        {
+            Guid boundaryGuid = new Guid();
+
+            HtmlToText converter = new HtmlToText();
+            string originalHtml = renderResult.MessageBody;
+            string textVersion = converter.ConvertHtml(originalHtml);
+
+            parameters.MailMessage.Headers["Content-Type"] = "multipart/alternative;boundary=" + boundaryGuid;
+
+            renderResult.MessageBody = "\r\n\r\n" + boundaryGuid + "\r\n\r\n";
+
+            renderResult.MessageBody += "Content-Type: text/plain; charset=utf-8\r\n\r\n";
+            //renderResult.MessageBody += "Content-Transfer-Encoding: base64\r\n";
+            renderResult.MessageBody += "\r\n" + textVersion + "\r\n";
+            renderResult.MessageBody += "\r\n\r\n--" + boundaryGuid + "\r\n";
+
+            renderResult.MessageBody += "Content-Type: text/html; charset=utf-8\r\n\r\n";
+            //renderResult.MessageBody += "Content-Transfer-Encoding: base64\r\n";
+            renderResult.MessageBody += "\r\n" + originalHtml + "\r\n";
+            renderResult.MessageBody += "\r\n\r\n--" + boundaryGuid + "\r\n";
+        }
+    }
+}
